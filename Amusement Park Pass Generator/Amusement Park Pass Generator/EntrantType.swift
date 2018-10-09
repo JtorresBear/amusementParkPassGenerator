3d0c730732e0152c7114d32132fa6003d23a6cb8
Juan Torres <jmmtorres1991@yahoo.com>
2018-10-09 18:27:16 -0500

    Renamed a file.

---
+++ Amusement Park Pass Generator/Amusement Park Pass Generator/EntrantType.swift
@@ -0,0 +1,17 @@
+//
+//  GuestType.swift
+//  Amusement Park Pass Generator
+//
+//  Created by Juan Torres on 10/9/18.
+//  Copyright © 2018 Juan Torres. All rights reserved.
+//
+
+import Foundation
+
+enum EntrantType
+{
+    case classicGuest
+    case vipGuest
+    case freeChildGuest
+    case hourlyEmployee
+}
