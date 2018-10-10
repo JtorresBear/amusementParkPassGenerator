3791404abe388a22e2f5e16ae5c577528de80dd4
Juan Torres <jmmtorres1991@yahoo.com>
2018-10-09 20:12:26 -0500

    Blank Start

---
+++ PassGenerator/PassGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassGenerator
+//
+//  Created by Juan Torres on 10/9/18.
+//  Copyright © 2018 Juan Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
